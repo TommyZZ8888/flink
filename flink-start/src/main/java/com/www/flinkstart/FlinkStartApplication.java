8951db3470ce2f9efe7ac77c352f7581653b125e
TommyZZ8888 <1950442495@qq.com>
2023-05-18 08:41:21 +0800

    flink创建

---
+++ flink-start/src/main/java/com/www/flinkstart/FlinkStartApplication.java
@@ -0,0 +1,13 @@
+package com.www.flinkstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkStartApplication.class, args);
+    }
+
+}
---
+++ flink-start/src/test/java/com/www/flinkstart/FlinkStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.flinkstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
